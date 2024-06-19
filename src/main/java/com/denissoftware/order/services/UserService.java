95d9ed792c4d762ea4837af2aac06c63a69dcd9d
Denis Macedo <denisrmacedo@gmail.com>
2024-06-19 15:03:42 -0300

    create user service, component registration

---
+++ src/main/java/com/denissoftware/order/services/UserService.java
@@ -0,0 +1,26 @@
+package com.denissoftware.order.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.denissoftware.order.entities.User;
+import com.denissoftware.order.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
