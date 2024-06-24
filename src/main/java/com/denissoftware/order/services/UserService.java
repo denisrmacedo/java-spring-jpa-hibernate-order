8205a6be7aeb32d6649ccf9e6a50b074f7ed55f6
Denis Macedo <denisrmacedo@gmail.com>
2024-06-23 21:15:07 -0300

    exception halndling findBuId

--- src/main/java/com/denissoftware/order/services/UserService.java
+++ src/main/java/com/denissoftware/order/services/UserService.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import com.denissoftware.order.entities.User;
 import com.denissoftware.order.repositories.UserRepository;
+import com.denissoftware.order.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
